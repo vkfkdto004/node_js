f495d96c2c9fc86def27eeb22d0557ea8d9cc180
wooseop.kim <125433456+vkfkdto004@users.noreply.github.com>
2023-10-06 23:46:04 +0900

    Add files via upload

---
+++ syntax/boolean.js
@@ -0,0 +1,11 @@
+// 1 -> 숫자
+// '1' -> 문자
+// boolean -> true, false
+console.log(true);
+console.log(false);
+a = 1;
+if (a == 100){
+    console.log('Yes'); // true
+} else {
+    console.log('NO!!'); // false 
+}
---
+++ syntax/comparsion.js
@@ -0,0 +1,6 @@
+console.log(1==1); // true
+console.log(1==2); // false
+console.log(1>2); // false
+console.log(1<2); // true
+console.log(1===1); // true  == -> 좌항과 우항이 같은가
+console.log(1===2); // false === -> 좌항과 우항이 정확하게 같은가
